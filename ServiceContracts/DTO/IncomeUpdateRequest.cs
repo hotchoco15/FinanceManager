1b6fe4061fd21240ee43ea52d0de6a40e125b3e7
S. YOO <124761766+hotchoco15@users.noreply.github.com>
2024-10-31 19:19:21 +0900

    Update IncomeUpdateRequest.cs

--- ServiceContracts/DTO/IncomeUpdateRequest.cs
+++ ServiceContracts/DTO/IncomeUpdateRequest.cs
@@ -10,22 +10,21 @@ namespace ServiceContracts.DTO
 	public class IncomeUpdateRequest
 	{
 		public Guid IncomeID { get; set; }
-
-
+	
 		[Required(ErrorMessage = "이름은 필수항목입니다.")]
 		public string? IncomeName { get; set; }
-
+	
 		[Required(ErrorMessage = "날짜는 필수항목입니다.")]
 		public DateTime? DateOfIncome { get; set; }
-
+	
 		[Required(ErrorMessage = "항목은 필수항목입니다.")]
 		public IncomeOptions? IncomeType { get; set; }
-
+	
 		[Required(ErrorMessage = "금액은 필수항목입니다. 숫자로 입력해주세요.")]
 		public double? IncomeAmount { get; set; }
-
+	
 		public string? IncomeRemark { get; set; }
-
+	
 		public Income ToIncome()
 		{
 			return new Income
