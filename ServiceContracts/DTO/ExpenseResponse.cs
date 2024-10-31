e5bd5c3a9239f9b3b7318b613c2db538146f48e0
S. YOO <124761766+hotchoco15@users.noreply.github.com>
2024-10-31 19:14:25 +0900

    Update ExpenseResponse.cs

--- ServiceContracts/DTO/ExpenseResponse.cs
+++ ServiceContracts/DTO/ExpenseResponse.cs
@@ -25,66 +25,66 @@ namespace ServiceContracts.DTO
         {
             return new ExpenseUpdateRequest()
             {
-				ExpenseID = ExpenseID,
-				DateOfExpense = DateOfExpense,
-				ExpenseName = ExpenseName,
-				ExpenseType = (ExpenseOptions)Enum.Parse(typeof(ExpenseOptions), ExpenseType, true),
-				ExpenseAmount = ExpenseAmount,
-				ExpenseRemark = ExpenseRemark
-			};
+		ExpenseID = ExpenseID,
+		DateOfExpense = DateOfExpense,
+		ExpenseName = ExpenseName,
+		ExpenseType = (ExpenseOptions)Enum.Parse(typeof(ExpenseOptions), ExpenseType, true),
+		ExpenseAmount = ExpenseAmount,
+		ExpenseRemark = ExpenseRemark
+	     };
         }
 
     }
 
     public static class ExpenseExtensions
-	{
+    {
         public static ExpenseResponse ToExpenseResponse(this Expense expense) 
         {
             return new ExpenseResponse()
             {
-				ExpenseID = expense.ExpenseID,
-				DateOfExpense = expense.DateOfExpense,
-				ExpenseName = expense.ExpenseName,
-				ExpenseType = expense.ExpenseType,
-				ExpenseAmount = expense.ExpenseAmount,
-				ExpenseRemark = expense.ExpenseRemark
-			};
+		ExpenseID = expense.ExpenseID,
+		DateOfExpense = expense.DateOfExpense,
+		ExpenseName = expense.ExpenseName,
+		ExpenseType = expense.ExpenseType,
+		ExpenseAmount = expense.ExpenseAmount,
+		ExpenseRemark = expense.ExpenseRemark
+	    };
         }
 
-		public static string GetDisplayNameofExpense(this string value)
+	public static string GetDisplayNameofExpense(this string value)
+	{
+		if (value == "Housing")
 		{
-			if (value == "Housing")
-            {
-                return "주거비";
-            }
-            else if (value == "Transportation")
-            {
-                return "교통비";
-			}
-			else if (value == "Grocery")
-			{
-				return "장보기";
-			}
-			else if (value == "Food")
-			{
-				return "외식비";
-			}
-			else if (value == "Shopping")
-			{
-				return "쇼핑";
-			}
-			else if (value == "InsuranceFee")
-			{
-				return "보험료";
-			}
-			else if (value == "Other")
-			{
-				return "기타";
-			}
-			else 
-            { 
-                return "없음"; 
-            }
+                	return "주거비";
+            	}
+	        else if (value == "Transportation")
+	        {
+			return "교통비";
 		}
+		else if (value == "Grocery")
+		{
+			return "장보기";
+		}
+		else if (value == "Food")
+		{
+			return "외식비";
+		}
+		else if (value == "Shopping")
+		{
+			return "쇼핑";
+		}
+		else if (value == "InsuranceFee")
+		{
+			return "보험료";
+		}
+		else if (value == "Other")
+		{
+			return "기타";
+		}
+		else 
+            	{ 
+                	return "없음"; 
+            	}
+	    }
 	}
 }
