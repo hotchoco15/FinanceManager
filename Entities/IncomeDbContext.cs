885c3e516f84e48b6cd4590a00a49f83d0dd63d7
S. YOO <124761766+hotchoco15@users.noreply.github.com>
2024-10-31 19:25:03 +0900

    Update IncomeDbContext.cs

--- Entities/IncomeDbContext.cs
+++ Entities/IncomeDbContext.cs
@@ -6,9 +6,7 @@ namespace Entities
 {
 	public class IncomeDbContext : DbContext
 	{
-		public IncomeDbContext(DbContextOptions options) : base(options)
-		{
-		}
+		public IncomeDbContext(DbContextOptions options) : base(options){}
 		public DbSet<Income> Incomes { get; set; }
 		public DbSet<Expense> Expenses { get; set; }
 
